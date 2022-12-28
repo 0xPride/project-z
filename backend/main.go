9b40cf0859f0634bb2a1987087e4baa624e7873c
Ayoub Eddaoudi <eddaouddi.ayoub@gmail.com>
2022-12-28 21:30:54 +0000

    add nweets to api url

--- backend/main.go
+++ backend/main.go
@@ -38,7 +38,7 @@ func main() {
 	d.AutoMigrate(&post.Nwita{})
 
 	db = d
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/nweets", handler)
 
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
